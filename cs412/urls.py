92e51e29948cf7562b729abde46b96a2920a7d58
Annelise Schreiber <aschreib@bu.edu>
2025-02-09 14:14:46 -0500

    Adding all files

--- cs412/urls.py
+++ cs412/urls.py
@@ -21,5 +21,6 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("quotes/", include("quotes.urls")) #new line
+    path("quotes/", include("quotes.urls")), #new line
+    path('', include("quotes.urls") )
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
